526e4e141846608000b94d29bbb8240f079f6b5f
gustavo lima <gustavolll15567@gmail.com>
2023-11-06 16:00:37 -0300

    16:00

--- conteudo/header.php
+++ conteudo/header.php
@@ -20,10 +20,19 @@
                     <li>
                         <a href="sobre.php">Sobre</a>
                     </li>
+                    <div>
+                        <li class="subMenu"><a href="produto.php" class="PRT">Produtos</a>
+                            <ul>
+                                <li><a href="#">Gelos</a></li>
+                                <li><a href="#">Sucos</a></li>
+                                <li><a href="#">Vinhos</a></li>
+                                <li><a href="#">Alcoólicas</a></li>
+                                <li><a href="#">Cervejas</a></li>
+                                <li><a href="#">Energéticos</a></li>
+                            </ul>
+                        </li>
+                    </div>
 
-                    <li>
-                        <a href="produto.pho" class="PRT">Produtos</a>
-                    </li>
                     <li>
                         <a href="contato.php">Contato</a>
                     </li>
