4d791f0b86f6a80f39dc63adfb491166b54c645f
NICOLAS HENRIQUE DA SILVA BRITO <nicolas.hsbrito@senacsp.edu.br>
2024-01-24 08:53:58 -0300

    front end do dashboard finalizado

---
+++ admin/class/clientes/listar.php
@@ -0,0 +1,23 @@
+<div class="ajusteTabela site">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Data de Nascimento</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Nicolas</td>
+                <td>04/12/2003</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Henrique</td>
+                <td>04/04/2004</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ admin/index.php
@@ -0,0 +1,192 @@
+<?php
+$paginaAtual = @$_GET['p'];
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@200&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        <div class="site">
+            <div class="header ">
+                <div class="logoTamanho  wow animate__animated animate__fadeInUp">
+                    <img class=" wow animate__animated animate__fadeInUp" src="../img/logo.png" alt="">
+                </div>
+
+                <button class="abrirMenu"></button>
+                <nav class="menu">
+
+
+                    <button class="fecharMenu"></button>
+
+                    <ul>
+                        <li>
+                            <a href="home.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="sobre.php">Sobre</a>
+                        </li>
+                        <div>
+                            <li class="subMenu"><a href="produto.php?p=Cervejas" class="PRT">Produtos</a>
+                                <ul>
+                                    <li><a href="produto.php?p=Gelos">Gelos</a></li>
+                                    <li><a href="produto.php?p=Sucos">Sucos</a></li>
+                                    <li><a href="produto.php?p=Vinhos">Vinhos</a></li>
+                                    <li><a href="produto.php?p=Alcoólicas">Alcoólicas</a></li>
+                                    <li><a href="produto.php?p=Cervejas">Cervejas</a></li>
+                                    <li><a href="produto.php?p=Energéticos">Energéticos</a></li>
+                                </ul>
+                            </li>
+                        </div>
+
+                        <li>
+                            <a href="contato.php">Contato</a>
+                        </li>
+                    </ul>
+
+                    <div class="user">
+                        <a href="formLogin.php">
+                            <img class=" wow animate__animated animate__fadeInUp" src="../img/user.png" alt="">
+                        </a>
+                    </div>
+                </nav>
+
+                <a href="formLogin.php">
+                    <img class=" wow animate__animated animate__fadeInUp" src="../img/user.png" alt="">
+                </a>
+
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="navegacao site">
+            <ul>
+
+                <li><a href="index.php?p=produtos" class="<?php echo (($paginaAtual == 'produtos') || ($paginaAtual == '')) ? 'menuAtivo' : '' ?>">Produtos</a></li>
+                <li><a href="index.php?p=funcionarios" class="<?php echo ($paginaAtual == 'funcionarios') ? 'menuAtivo' : '' ?>">Funcionários</a></li>
+                <li><a href="index.php?p=clientes" class="<?php echo ($paginaAtual == 'clientes') ? 'menuAtivo' : '' ?>">Clientes</a></li>
+                <li><a href="index.php?p=fornecedores" class="<?php echo ($paginaAtual == 'fornecedores') ? 'menuAtivo' : '' ?>">Fornecedores</a></li>
+                <li><a href="index.php?p=compras" class="<?php echo ($paginaAtual == 'compras') ? 'menuAtivo' : '' ?>">Compras</a></li>
+                <li><a href="index.php?p=email" class="<?php echo ($paginaAtual == 'email') ? 'menuAtivo' : '' ?>">E-mail</a></li>
+                <li><a href="index.php?p=ajuda" class="<?php echo ($paginaAtual == 'ajuda') ? 'menuAtivo' : '' ?>">Ajuda & Suporte</a></li>
+            </ul>
+        </div>
+
+        <h1><?php
+            switch ($paginaAtual) {
+                case 'produtos':
+                    echo 'Produtos';
+                    break;
+                case 'funcionarios':
+                    echo 'Funcionários';
+                    break;
+                case 'clientes':
+                    echo 'Clientes';
+                    break;
+                case 'fornecedores':
+                    echo 'Fornecedores';
+                    break;
+                case 'compras':
+                    echo 'Compras';
+                    break;
+                case 'email':
+                    echo 'E-mail';
+                    break;
+                case 'ajuda':
+                    echo 'Ajuda & Suporte';
+                    break;
+
+                default:
+                    echo 'Produtos';
+                    break;
+            }
+            ?>
+        </h1>
+
+        <div class="box">
+        <?php
+            switch ($paginaAtual) {
+                case 'produtos':
+                    echo 'Produtos';
+                    break;
+                case 'funcionarios':
+                    echo 'Funcionários';
+                    break;
+                case 'clientes':
+                    require_once ('class/clientes/listar.php');
+                    break;
+                case 'fornecedores':
+                    echo 'Fornecedores';
+                    break;
+                case 'compras':
+                    echo 'Compras';
+                    break;
+                case 'email':
+                    echo 'E-mail';
+                    break;
+                case 'ajuda':
+                    echo 'Ajuda & Suporte';
+                    break;
+
+                default:
+                    echo 'Produtos';
+                    break;
+            }
+            ?>
+        </div>
+    </main>
+
+    <footer>
+        <section>
+            <div class="site centralizar">
+                <div class="estruturaRodape wow animate__animated animate__fadeInUp">
+                    <img src="../img/logo.png" alt="">
+                    <div class="contatos">
+
+                        <a href="mailto:ti21.smpsistema@senac.com">ti21.smpsistema@senac.com</a>
+                        <a href="tel:+55(11)952752175">(11)95275-2175</a>
+                    </div>
+                    <ul>
+                        <li><a href="https://ig.me/m/@adegadocleitinho"><img src="../img/insta40.png" alt=""></a></li>
+                        <li><a href="https://wa.me/5511952752175?text=aprecie+sua+bebida+conosco"><img src="./img/whats40.png" alt=""></a></li>
+                    </ul>
+                </div>
+
+                <div class="estruturaPagamento wow animate__animated animate__fadeInUp">
+                    <div class="fundoPagamento">
+                        <img src="../img/fotosCartao.png" alt="">
+                    </div>
+                    <div class="local">
+                        <h2>R. Manoel Preto, 100</h2>
+                        <h3>Itaquá - SP</h3>
+                        <h4>08593-130</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="desenvolvido wow animate__animated animate__fadeInUp">
+            <h2>Desenvolvido por LekDev</h2>
+        </section>
+        <section class="nomes wow animate__animated animate__fadeInUp">
+            <h2>Gustavo Lima e Nicolas Henrique</h2>
+        </section>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>       
+
+</body>
+
+</html>
